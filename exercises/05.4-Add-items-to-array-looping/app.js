9e9d775cf5ebf6039e03a2bc7d810736750ba537
Jose <josejesuscedeno@gmail.com>
2022-02-14 16:48:18 +0000

    06.3 Loop adding two

--- exercises/05.4-Add-items-to-array-looping/app.js
+++ exercises/05.4-Add-items-to-array-looping/app.js
@@ -1,12 +1,8 @@
 var arr = [4,5,734,43,45];
 
-//*****************
-// Your code here
-// you need to loop 10 times, for example, using a for loop
-//*****************
-let a = 4
+for(let i=0;i<10;i++){
+    arr.push(Math.floor(Math.random()*100))
+    ;}
 
-for(i=1;i<11;i++){
-    arr.push(Math.floor(Math.random()*10), Math.floor(Math.random()*10));}
 
-console.log(arr)
+    console.log(arr)

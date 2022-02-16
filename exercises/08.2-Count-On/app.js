3a8fd8ead4269a76d3f2ad6c770db8d6b8cafbb8
Jose <josejesuscedeno@gmail.com>
2022-02-16 22:52:06 +0000

    kkkk

--- exercises/08.2-Count-On/app.js
+++ exercises/08.2-Count-On/app.js
@@ -1,9 +1,14 @@
 let myArray = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
 
 let hello = [];
-for(let index = 0; index < myArray.length; index++){
-    let element = myArray[index];
-    // MAGIC HAPPENS HERE
-}
+for(let i = 0; i < myArray.length; i++){
+    let element = myArray[i];
+    if ((myArray[i]) = Object ) {hello.push(typeof element[i])}
+    
+  
 
-console.log(hello)
+    
+};
+
+
+console.log(hello);

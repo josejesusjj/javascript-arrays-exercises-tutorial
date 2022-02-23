120a6759d19a106c1722ce3846e4cd12d4660d06
Jose <josejesuscedeno@gmail.com>
2022-02-23 23:13:02 +0000

    nose

--- .learn/resets/12-Delete-element/app.js
+++ .learn/resets/12-Delete-element/app.js
@@ -1,9 +1,6 @@
 var people = ['juan','ana','michelle','daniella','stefany','lucy','barak', 'emilio'];
 
 //your code below
-let deletePerson = people.filter((element) => element === 'd' || element === 'juan' || element === 'emilio' || element === 'daniella' || element === 'juan' );
-console.log(deletePerson);
-//hasta aqui
 
 console.log(deletePerson('daniella'));
 console.log(deletePerson('juan'));
--- exercises/11-Do-while/app.js
+++ exercises/11-Do-while/app.js
@@ -2,6 +2,7 @@ let i = 20;
 
 do {
     // Magic goes here;
-    
+    if (i==0) {console.log("LIFTOFF")} else {if (i%5===0){console.log(i+"!")} else console.log(i)}
+    //hasta aqui
     i--;
-} while (i > 0);
+} while (i >= 0);
